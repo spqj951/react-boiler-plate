78327c01c9435d1c7d982f5855e58a18c8a8ef24
kyujin <spqj951@gmail.com>
2022-07-24 15:20:27 +0900

    git ignore

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://kyujin:kor2an4m2@boilerplate.hr4qvkr.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("hello world"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
